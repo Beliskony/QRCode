c9cbc1a8b796dfc7b6c6a369121d7de2742427c4
Axel NGuessan <nguessanaxel21@gmail.com>
2025-04-14 11:32:36 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 
 function App() {
   return (
+    <section className='h-screen w-screen justify-center items-center flex'>
      <Qr crea={"https://www.bibebook.com/files/ebook/libre/V2/segond_louis_-_la_sainte_bible.pdf"} />
+     </section>
   )
 }
 
