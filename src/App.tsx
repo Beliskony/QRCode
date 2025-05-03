b505e22d4ceb7e12a73e988e61fd584ecba81829
Axel NGuessan <nguessanaxel21@gmail.com>
2025-05-03 15:31:58 +0000

    ma page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
     <section className='h-screen w-screen justify-center items-center flex'>
-     <Qr crea={"https://www.bibebook.com/files/ebook/libre/V2/segond_louis_-_la_sainte_bible.pdf"} />
+     <Qr crea={"https://virtualcarte.vercel.app/"} />
      </section>
   )
 }
